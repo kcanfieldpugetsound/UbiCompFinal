cd52214efd96e3bd4d32ab1274510426e8d38d41
kcanfieldpugetsound <kcanfield@mac.com>
2016-04-16 15:40:54 -0400

    adds android studio project

---
+++ UbiCompFinal/app/src/main/java/edu/cornell/kramercanfield/ubicompfinal/MainActivity.java
@@ -0,0 +1,58 @@
+//Copyright (c) Microsoft Corporation All rights reserved.
+//
+//MIT License:
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+//documentation files (the  "Software"), to deal in the Software without restriction, including without limitation
+//the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+//to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+//the Software.
+//
+//THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+//TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+//CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+//IN THE SOFTWARE.
+
+package edu.cornell.kramercanfield.ubicompfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.microsoft.band.BandClient;
+import com.microsoft.band.BandClientManager;
+import com.microsoft.band.BandException;
+import com.microsoft.band.BandInfo;
+import com.microsoft.band.BandIOException;
+import com.microsoft.band.ConnectionState;
+import com.microsoft.band.sensors.BandAccelerometerEvent;
+import com.microsoft.band.sensors.BandAccelerometerEventListener;
+import com.microsoft.band.sensors.SampleRate;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BandClient client = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    private BandAccelerometerEventListener mAccelerometerEventListener = new BandAccelerometerEventListener() {
+        @Override
+        public void onBandAccelerometerChanged(final BandAccelerometerEvent event) {
+//            if (event != null) {
+//                appendToUI(String.format(" X = %.3f \n Y = %.3f\n Z = %.3f", event.getAccelerationX(),
+//                        event.getAccelerationY(), event.getAccelerationZ()));
+//            }
+        }
+    };
+
+
+}
